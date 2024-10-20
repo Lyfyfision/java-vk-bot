9b1d87c59e152d14e64f3e47f8fdd5f5f3b095f1
Lyfyfision <103595446+Lyfyfision@users.noreply.github.com>
2024-10-20 18:34:32 +0300

    Readme edited

--- src/main/java/com/bots/vkbot/utils/VkApiConstants.java
+++ src/main/java/com/bots/vkbot/utils/VkApiConstants.java
@@ -3,8 +3,10 @@ package com.bots.vkbot.utils;
 public class VkApiConstants {
     public static final String API_URL = "https://api.vk.com/method/";
     public static final String API_VERSION = "5.81";
-    public static final String ACCESS_TOKEN = "vk1.a.LUq8YGnjeb6s2FBbuh7stWkLihf2nry0ZmGCQFLEFN-xF1Cxo2Xyx__BiTnGTRD3semCxn0T_2bT8EaAMUFjJ_iRI7lkFyCgZQF8jiTikSX0JC09f0V-aRogmPeJqoBnbfL1wxGDRUrNnvM8JlEWLa7qIBNAiBWZc3bM4Hh71yExUzMxXrEMpcZOSmlEZJSm5BLVIIFCtqtByRFc1ytB8Q";
-    public static final String CONFIRMATION_TOKEN = "68e664df";
+    //Insert here your access token from community setting
+    public static final String ACCESS_TOKEN = " ";
+    //Insert here your confirmation token from community setting
+    public static final String CONFIRMATION_TOKEN = " ";
     private VkApiConstants() {
         throw new UnsupportedOperationException("Do not init a utility class");
     }
